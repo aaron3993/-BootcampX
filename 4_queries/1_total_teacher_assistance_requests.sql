7ea2bc43297e9058974aa7b293b21d0be7c12f8e
Aaron Sham <aaron@Aarons-MacBook-Air.local>
2020-06-23 13:59:36 -0400

    Completed all 4_queries queries

--- 4_queries/1_total_teacher_assistance_requests.sql
+++ 4_queries/1_total_teacher_assistance_requests.sql
@@ -1,15 +1,5 @@
-sum(assistance_requests.*) as total_assistances, name
+SELECT count(assistance_requests.*) as total_assistances, teachers.name
 FROM assistance_requests
-JOIN teachers ON teacher_id = teachers.id
+JOIN teachers ON teachers.id = teacher_id
 WHERE name = 'Waylon Boehm'
-
-Get the total number of assistance_requests for a teacher.
-
-Select the teacher's name and the total assistance requests.
-Since this query needs to work with any specific teacher name, use 'Waylon Boehm' for the teacher's name here.
-Expected Result:
-
- total_assistances |     name     
--------------------+--------------
-              4227 | Waylon Boehm
-(1 row)
+GROUP BY teachers.name;
